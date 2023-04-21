0cc124d672b25ed549f710a0c882884640de008f
lamnguyen <nguyenphuonglam96@gmail.com>
2023-04-21 20:14:01 +0700

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,32 @@
+const { JSDOM } = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = []
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) == '/') {
+            const link = baseURL+ linkElement.href;
+            try {
+                const urlObj = new URL(link);
+                urls.push(urlObj.href);
+            } catch (err) {
+                console.log(`Error: ${err}`);
+            }
+        }
+        else {
+            try {
+                const urlObj = new URL(linkElement.href)
+                urls.push(urlObj.href);
+            } catch(err) {
+                console.log(`Error: ${err}`);
+            }
+        }
+        console.log(linkElement.href);
+    }
+    return urls;
+}
+
 function normalizeURL(urlString) {
     const urlObj = new URL(urlString);
     let result = urlObj.hostname + urlObj.pathname;
@@ -14,7 +43,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL, 
+    getURLsFromHTML
 }
-
-normalizeURL('https://blog.boot.dev/path/')
--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log('hello world')
+
+const test = new URL('/path/')
+console.log(test.hostname)
+ console.log(test.pathname)
